979423502c4e746b07b96337246ed68ade55e740
Jaehong Kang <sinoru@me.com>
2026-03-04 14:20:12 +0900

    Initial Commit

---
+++ PatronArchiver/PatronArchiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatronArchiverApp.swift
+//  PatronArchiver
+//
+//  Created by 강재홍 on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct PatronArchiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PatronArchiverTests/PatronArchiverTests.swift
@@ -0,0 +1,16 @@
+//
+//  PatronArchiverTests.swift
+//  PatronArchiverTests
+//
+//  Created by 강재홍 on 3/4/26.
+//
+
+import Testing
+
+struct PatronArchiverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
